721f3dbba5f2a51a508ead824ab7566d9c7e1bd6
unknown <thanh.van19062004@gmail.com>
2024-07-21 16:52:21 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import BT1 from "./bt1/bt1";
+// import BT1 from "./bt1/bt1";
 import BT4 from "./bt4/BT4";
 import Login from "./bt4/login";
-import BT2 from "./bt2/bt2";
+// import BT2 from "./bt2/bt2";
 import Home from "./bt4/pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/bt1" element={<BT1 />} />
-      <Route path="/bt2" element={<BT2 />} />
+      {/* <Route path="/bt1" element={<BT1 />} /> */}
+      {/* <Route path="/bt2" element={<BT2 />} /> */}
       {/* <Route path="/bt3" element={<BT3 />} /> */}
       <Route path="/bt4" element={<BT4 />} >
       <Route path="login" element={<Login />} />
