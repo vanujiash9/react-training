4db8d9de320381fb014c6cd345857333a6126ccd
unknown <thanh.van19062004@gmail.com>
2024-07-21 16:31:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import BT3 from "./bt3/bt3";
+import BT1 from "./bt1/bt1";
+import BT4 from "./bt4/BT4";
+import Login from "./bt4/login";
+import BT2 from "./bt2/bt2";
+import Home from "./bt4/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/bt1" element={<BT1 />} />
+      <Route path="/bt2" element={<BT2 />} />
+      <Route path="/bt3" element={<BT3 />} />
+      <Route path="/bt4" element={<BT4 />} >
+      <Route path="login" element={<Login />} />
+        <Route path="about" element={<Login />} />
+        <Route path="home" element={<Home/>} />
+        <Route path="register" element={<Login />} />
+        <Route path="product" element={<Login />} />
+      </Route>
+    </Routes>
   );
 }
 
